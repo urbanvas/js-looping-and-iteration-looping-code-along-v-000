f9cba370fd9f558ea198e1bbdbe5552836fffba7
Neil Seejoor <nvseejoor@gmail.com>
2018-10-02 21:31:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+function printBadges(arr) {
+  for (let i = 0, i < arr.length, i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`)
+  }
+}
+
+function tailsNeverFails() {
+
+}
