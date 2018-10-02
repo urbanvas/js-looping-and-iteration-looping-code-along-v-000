0fecace69ce2212dde47f3b56f85f1bfeefc3f11
Neil Seejoor <nvseejoor@gmail.com>
2018-10-02 21:44:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 
 function printBadges(arr) {
-  for (let i = 0, i < arr.length, i++) {
+  for (let i = 0; i < arr.length; i++) {
     console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`)
   }
   return arr
