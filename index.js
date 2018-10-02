5cdf29422345ffd2b96c0472f97438ce6f2b1920
Neil Seejoor <nvseejoor@gmail.com>
2018-10-02 21:41:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function printBadges(arr) {
   for (let i = 0, i < arr.length, i++) {
     console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`)
   }
+  return arr
 }
 
 function tailsNeverFails() {
